ecc503c7139fac5026e1152c49af2a93cd45654c
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-06 11:19:38 -0400

    more things

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import logging
+import os
+import sys
+
 from flask import Flask, request
 app = Flask(__name__)
 
+GROUPME_API_URL = 'https://api.groupme.com/v3/bots/post'
 
 @app.route('/')
 def hello_world():
@@ -11,9 +16,20 @@ def hello_world():
 @app.route('/webhook/', methods=['POST'])
 def webhook():
     data = request.get_json()
-    print(data)
+    app.logger.debug(data)
 
     return 'OK', 200
 
+
+def send_message(msg):
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text' : msg,
+    }
+    app.logger.debug(data)
+
+
 if __name__ == '__main__':
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.DEBUG)
     app.run()
